c3d70bdb961df6cf8ff018700d21dd670e70445e
Sarah Currey <curreysarah28@gmail.com>
2018-11-26 15:46:01 -0500

    footer added

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -7,35 +7,55 @@ function Footer() {
     <div>
         <footer>
         <div className="footer">
-            <div className="airbnbFooter">
-             <h6> Airbnb </h6>
+            <div className="amazonFooter">
+             <h6 className="boldFoot"> Get to know us </h6>
              <p> Careers </p>
-             <p> Press</p>
+             <p> About Us</p>
              <p> Policies</p>
              <p> Help </p>
              <p> Diversity & Belonging</p>
             </div>
 
             <div className="discoverFooter">
-             <h6> Discover </h6>
-             <p> Trust & Safety </p>
-             <p> Travel Credit</p>
-             <p> Gift Cards</p>
-             <p> Airbnb Citizen </p>
-             <p> Business Travel </p>
+             <h6 className="boldFoot"> Make money with us </h6>
+             <p> Sell on Amazon </p>
+             <p> Sell on Amazon Business</p>
+             <p> Associates Programme</p>
+             <p> Fulfillment by Amazon </p>
+             <p> Amazon Pay </p>
+             <p> Become an Amazon Vendor </p>
             </div>
 
             <div className="hostingFooter">
-             <h6> Hosting </h6>
-             <p> Hospitality </p>
-             <p> Community Center</p>
-             <p> Open Homes </p>
+             <h6 className="boldFoot"> Amazon Payments </h6>
+             <p> Gift Cards </p>
+             <p> Amazon Money Store</p>
+             <p> Amazon Currency Converter </p>
+             <p> Shop with Points</p>
+             <p> Top your Account</p>
+             <p> Payment Methods Help</p>
             </div>
 
-            <div className="termsFooter">
-             <p> Terms </p>
-             <p> Privacy</p>
-             <p> Site Map </p>
+            <div className="hostingFooter">
+             <h6 className="boldFoot"> Let us help you </h6>
+             <p> Track Packages</p>
+             <p> Amazon Prime </p>
+             <p> Returns & Replacements </p>
+             <p> Manage your Content </p>
+             <p> Amazon Mobile App</p>
+             <p> Amazon Assitant</p>
+             <p> Delivery Rates </p>
+            </div>
+
+            <div className="hostingFooter">
+             <h6 className="boldFoot"> Other Brands </h6>
+             <p> Amazon Music</p>
+             <p> Amazon Tickets </p>
+             <p> DPR Review </p>
+             <p> Kindle direct </p>
+             <p> Souq.com</p>
+             <p> Abebooks.com</p>
+             <p> More... </p>
             </div>
             </div>
         </footer>
